8e73f270bbe3e041ac2469e8dfa49ea679bdaaa8
Mingxuan Xiang <mx_xiang@hust.edu.cn>
2023-09-21 13:11:50 +0800

    change to the encrypted login method

--- login.go
+++ login.go
@@ -4,12 +4,23 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"math/big"
 	"net/http"
 	"net/url"
 	"os"
+	"regexp"
 	"strconv"
 	"strings"
 )
+
+func encrypt(input string) string {
+	e := 65537
+	n, _ := new(big.Int).SetString("94dd2a8675fb779e6b9f7103698634cd400f27a154afa67af6166a43fc26417222a79506d34cacc7641946abda1785b7acf9910ad6a0978c91ec84d40b71d2891379af19ffb333e7517e390bd26ac312fe940c340466b4a5d4af1d65c3b5944078f96a1a51a5a53e4bc302818b7c9f63c4a1b07bd7d874cef1c3d4b2f5eb7871", 16)
+	m := new(big.Int).SetBytes([]byte(input))
+	enc := new(big.Int).Exp(m, big.NewInt(int64(e)), n)
+	ret := fmt.Sprintf("%0256s", enc.Text(16))
+	return ret
+}
 
 func (c *Config) loginLoop(doLoginReqChan chan bool, resultChan chan int) {
 	for {
@@ -56,13 +67,18 @@ func (c *Config) login(idx int) int {
 	}
 	cookie := cookieResp.Header.Get("Set-Cookie")
 	fmt.Println(cookie)
-
+	mac_reg := regexp.MustCompile(`mac=([0-9a-f]+)&`)
+	mac_match := mac_reg.FindStringSubmatch(formData)
+	if len(mac_match) != 2 {
+		return ERR_LOGIN_FAILED
+	}
+	mac := mac_match[1]
 	form := url.Values{}
 	form.Add("userId", c.Accounts[idx].Username)
-	form.Add("password", c.Accounts[idx].Password)
+	form.Add("password", encrypt(fmt.Sprintf("%s>%s", c.Accounts[idx].Password, mac)))
 	form.Add("service", "")
 	form.Add("queryString", formData)
-	form.Add("passwordEncrypt", "false")
+	form.Add("passwordEncrypt", "true")
 
 	client := http.Client{}
 	req, _ := http.NewRequest("POST", "http://192.168.50.3:8080/eportal/InterFace.do?method=login", strings.NewReader(form.Encode()))
